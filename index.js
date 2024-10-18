87e10d9445cea1057a9de4bd018000f3449a3f9c
adrxan45 <151868816+adrxan45@users.noreply.github.com>
2024-10-18 22:35:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.listen(port, () => {
 });
 
 const statusMessages = ["🔗 dsc.gg/dilo"];
-const statusTypes = [ 'dnd', 'idle'];
+const statusTypes = [ 'dnd'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 1;
 
